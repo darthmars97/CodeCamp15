e7cb3a04f8bf8e0641a39daed582239a49ae2981
SoulxDragon <Soulxdragon@gmail.com>
2015-11-13 15:42:46 -0700

    Basic class structure.

---
+++ spookmath.py
@@ -0,0 +1,18 @@
+"""
+This is where the Math/graph numbers should be happening.
+"""
+
+
+class Song(object):
+    """docstring for Song"""
+    def __init__(self, name, popularity, value):
+        super(Song, self).__init__()
+        self.name = name
+        self.popularity = popularity
+        self.value = value
+
+    def pop_formula(self):
+        pass
+song = Song("Rolling in the deep - Adele", 50, 10)
+
+print song.popularity
