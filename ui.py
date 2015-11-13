ca7ec514c489574ce2db4ddb27ced9f1bc0451e8
SoulxDragon <Soulxdragon@gmail.com>
2015-11-13 10:07:52 -0700

    Trying to work around pygame's obtuse ways.

--- ui.py
+++ ui.py
@@ -1,1 +1,11 @@
 import pygame
+"""
+This is where the window is supposed to be drawn.... (I hope!)
+"""
+
+def draw_window():
+    screenWidth = 400
+    screenHeight = 700
+    screen = pygame.display.set_mode(screenWidth, screenHeight)
+
+draw_window()
